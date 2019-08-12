04a3f103a11290d4e631119262b421e98eae6a86
leiming <2008weileiming@gmail.com>
2019-08-12 11:03:08 +0800

    Update zentao

--- zentao:zentao/docker_zentao/docker-entrypoint.sh
+++ zentao:zentao/docker_zentao/docker-entrypoint.sh
@@ -10,6 +10,12 @@ if [ "`cat /app/zentaopms/VERSION`" != "`cat /var/www/zentaopms/VERSION`" ]; the
   cp -a /var/www/zentaopms/* /app/zentaopms
 fi
 
+chmod -R 777 /app/zentaopms/www/data
+chmod -R 777 /app/zentaopms/tmp
+chmod 777 /app/zentaopms/www
+chmod 777 /app/zentaopms/config
+chmod -R a+rx /app/zentaopms/bin/*
+
 /etc/init.d/apache2 start
 
 chown -R www-data:www-data /app/zentaopms
